32efbeac7f4d3ea8169752f0b2fe35637f50d162
jaune-attend64 <martel.jonathan64@gmail.com>
2024-03-18 16:19:25 +0100

    maj

---
+++ movies.php
@@ -0,0 +1,399 @@
+<?php
+include 'Config/database.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <?php include '_partials/head.php'; ?>
+
+<body>
+
+<?php include '_partials/header.php'; ?>
+
+<main>
+        <img src="./assets/image/movie_theatre/Slide/batverse.png" alt="" class="mt-3 slide">
+        <div>
+            <h1 class="text-uppercase bold text-center titre py-1">la page des cinéphiles</h1>
+        </div>
+        <div>
+            <h2 class="text-uppercase gras text-center py-2 stitre">bandes-annonces à ne pas manquer</h2>
+        </div>
+        <div class="text-center">
+
+            <section>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd1.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd2.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd3.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd4.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd5.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd6.png" alt="bandes-annonces"></a></article>
+            </section>
+        </div>
+        <div>
+            <div>
+                <h2 class="text-uppercase gras text-center py-2 stitre">actuellement disponible</h2>
+            </div>
+            <div class="black hashtag text-center">
+                <a href=""><p class="d-inline px-3"><span style="color:#F4661B">#</span> Films pour enfants</p></a>
+                <a href=""><p class="d-inline px-3"><span style="color:#F4661B">#</span> Agenda des sorties</p></a>
+                <a href=""><p class="d-inline px-3"><span style="color:#F4661B">#</span> Sorties DVD Blue-Ray & VOD du mois</p></a>
+                <a href=""><p class="d-inline px-3"><span style="color:#F4661B">#</span> Actus sorties</p></a>
+                <a href=""><p class="d-inline px-3"><span style="color:#F4661B">#</span> Box Office</p></a>
+            </div>
+
+
+
+            <section>
+          <div>
+            <form class="row">
+              <div class="col-6">
+                <label class="form-label"></label>
+                <input type="text" class="form-control light " placeholder="Rechercher une salle : Dax, CGR, 64100,..."
+                  aria-label="name" aria-describedby="basic-addon1">
+              </div>
+              <div class="col-6">
+                <label class="form-label"></label>
+                <p> <a href="./login.php" style="color:#434343">Connectez vous pour enregistrer vos films favoris</a></p> 
+              </div>
+              <div class="col-12">
+
+              </div>
+
+            </form>
+          </div>
+
+        </div>
+      </div>
+            <div class="py-4">
+             
+                <section class="text-center actu">
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd1.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd2.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd3.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd4.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd5.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd6.png" alt="bandes-annonces"></a></article>
+            </section>
+            </div>
+        </div>
+        <div>
+            <div>
+                <h2 class="text-uppercase gras text-center py-2 stitre">prochainement</h2>
+            </div>
+            <div class="py-4">
+                <section class="text-center actu">
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd1.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd2.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd3.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd4.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd5.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd6.png" alt="bandes-annonces"></a></article>
+            </section>
+            </div>
+        </div>
+        <div>
+            <div>
+                <h2 class="text-uppercase gras text-center py-2 stitre">les plus attendus</h2>
+            </div>
+            <div class="py-4">
+                <section class="text-center actu">
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd1.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd2.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd3.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd4.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd5.png" alt="bandes-annonces"></a></article>
+                <article class="d-inline"><a href=""><img src="./assets/image/movie_theatre/movies/bd6.png" alt="bandes-annonces"></a></article>
+            </section>
+            </div>
+        </div>
+    </main>
+
+
+<!-- <main>
+
+
+
+
+
+  <section>
+    <div class="fluid-container col-12 col-lg-12">
+      <div class="col-12 col-lg-12">
+        <div>
+          <p class="h2 uppercase bold "> Actuellement en salles</p>
+          <div class="h3 uppercase bold">
+            <p class="d-inline uppercase "> <span style="color:#F4661B">#</span> Films Marvel</p>
+            <p class="d-inline uppercase"> <span style="color:#F4661B;">#</span>Agenda des sorties</p>
+            <p class="d-inline uppercase"> <span style="color:#F4661B;">#</span>Sorties ciné du mois</p>
+            <p class="d-inline uppercase"> <span style="color:#F4661B;">#</span>Actus cinéma</p>
+            <p class="d-inline uppercase"> <span style="color:#F4661B;">#</span>Box Office</p>
+          </div>
+
+        
+
+
+          <div>
+            <form class="row col-xl-8 mx-auto">
+              <div class="col-12 col-xl-8 mb-4">
+                <label class="form-label"></label>
+                <input type="text" class="form-control light " placeholder="Rechercher une salle : Dax, CGR, 64100,..."
+                  aria-label="name" aria-describedby="basic-addon1">
+              </div>
+              <div class="col-12 col-xl-8 mb-lg-4">
+                <label class="form-label"></label>
+                <input type="text" class="form-control" placeholder="Connectez vous pour enregistrer vos films favoris">
+              </div>
+
+
+            </form>
+          </div>
+
+        </div>
+      </div>
+
+
+
+      <div class="col-12 col-lg-12 mb-4">
+        <div class="d-flex flex-row">
+          <div>
+            <p class="h3 uppercase bold">les sorties de la semaine</p>
+          </div>
+          <div class="col-md-1">
+            <img src="./assets/image/movie_theatre/Movies/antman.jpg" class="rounded-5" alt="antman" width="170"
+              height="230" id="box-shadow">
+          </div>
+          <div class="col-md-1">
+            <img src="./assets/image/movie_theatre/Movies/antman2.jpg" class="rounded-5" alt="antman2" width="170"
+              height="230px" id="box-shadow">
+          </div>
+          <div class="col-md-1">
+            <img src="./assets/image/movie_theatre/Movies/avengers.jpg" class="rounded-5" alt="avengers" width="170"
+              height="230px" id="box-shadow">
+          </div>
+          <div class="col-md-1">
+            <img src="./assets/image/movie_theatre/Movies/avengers2.jpg" class="rounded-5" alt="avengers2" width="170"
+              height="230px" id="box-shadow">
+          </div>
+        </div>
+      </div>
+
+
+    
+
+      <div class="col-12 col-lg-12 mb-4">
+
+        <div class="d-flex flex-row-reverse">
+          <div>
+            <p class="h3 uppercase bold">toujours à l'affiche</p>
+          </div>
+          <div class="col-md-1">
+            <img src="./assets/image/movie_theatre/Movies/antman.jpg" class="rounded-5" alt="antman" width="170"
+              height="230" id="box-shadow">
+          </div>
+          <div class="col-md-1">
+            <img src="./assets/image/movie_theatre/Movies/antman2.jpg" class="rounded-5" alt="antman2" width="170"
+              height="230px" id="box-shadow">
+          </div>
+          <div class="col-md-1">
+            <img src="./assets/image/movie_theatre/Movies/avengers.jpg" class="rounded-5" alt="avengers" width="170"
+              height="230px" id="box-shadow">
+          </div>
+          <div class="col-md-1">
+            <img src="./assets/image/movie_theatre/Movies/avengers2.jpg" class="rounded-5" alt="avengers2" width="170"
+              height="230px" id="box-shadow">
+          </div>
+        </div>
+      </div>
+
+
+
+      <div class="col-12 col-lg-12 mb-4">
+        <div class="d-flex flex-row">
+          <div>
+            <p class="h3 uppercase bold">prochainnement</p>
+          </div>
+          <div class="col-md-1">
+            <img src="./assets/image/movie_theatre/Movies/antman.jpg" class="rounded-5" alt="antman" width="170"
+              height="230" id="box-shadow">
+          </div>
+          <div class="col-md-1">
+            <img src="./assets/image/movie_theatre/Movies/antman2.jpg" class="rounded-5" alt="antman2" width="170"
+              height="230px" id="box-shadow">
+          </div>
+          <div class="col-md-1">
+            <img src="./assets/image/movie_theatre/Movies/avengers.jpg" class="rounded-5" alt="avengers" width="170"
+              height="230px" id="box-shadow">
+          </div>
+          <div class="col-md-1">
+            <img src="./assets/image/movie_theatre/Movies/avengers2.jpg" class="rounded-5" alt="avengers2" width="170"
+              height="230px" id="box-shadow">
+          </div>
+        </div>
+      </div>
+    
+
+      <div class="col-12 col-lg-12 mb-4">
+        <div class="d-flex flex-row-reverse">
+          <div>
+            <p class="h3 uppercase bold">les plus attendus par les fans</p>
+          </div>
+          <div class="col-md-1">
+            <img src="./assets/image/movie_theatre/Movies/antman.jpg" class="rounded-5" alt="antman" width="170"
+              height="230" id="box-shadow">
+          </div>
+          <div class="col-md-1">
+            <img src="./assets/image/movie_theatre/Movies/antman2.jpg" class="rounded-5" alt="antman2" width="170"
+              height="230px" id="box-shadow">
+          </div>
+          <div class="col-md-1">
+            <img src="./assets/image/movie_theatre/Movies/avengers.jpg" class="rounded-5" alt="avengers" width="170"
+              height="230px" id="box-shadow">
+          </div>
+          <div class="col-md-1">
+            <img src="./assets/image/movie_theatre/Movies/avengers2.jpg" class="rounded-5" alt="avengers2" width="170"
+              height="230px" id="box-shadow">
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
+
+  <section>
+    <div>
+      <p class="h2 bold uppercase"> DVD & BLU-RAY</p>
+      <p class="h3 uppercase bold"> LES SORTIES DU MOMENT</p>
+      <div class="d-flex flex-row"">
+          <div class=" col-md-1">
+        <img src="./assets/image/movie_theatre/Movies/antman.jpg" class="rounded-5" alt="antman" width="170"
+          height="230" id="box-shadow">
+      </div>
+      <div class="col-md-1">
+        <img src="./assets/image/movie_theatre/Movies/antman2.jpg" class="rounded-5" alt="antman2" width="170"
+          height="230px" id="box-shadow">
+      </div>
+      <div class="col-md-1">
+        <img src="./assets/image/movie_theatre/Movies/avengers.jpg" class="rounded-5" alt="avengers" width="170"
+          height="230px" id="box-shadow">
+      </div>
+      <div class="col-md-1">
+        <img src="./assets/image/movie_theatre/Movies/avengers2.jpg" class="rounded-5" alt="avengers2" width="170"
+          height="230px" id="box-shadow">
+      </div>
+
+
+      <p class="h3 uppercase bold">POUR LES ENFANTS</p>
+      <div class="d-flex flex-row-reverse ">
+      </div>
+      <div class="col-md-1">
+        <img src="./assets/image/movie_theatre/Movies/antman.jpg" class="rounded-5" alt="antman" width="170"
+          height="230" id="box-shadow">
+      </div>
+      <div class="col-md-1">
+        <img src="./assets/image/movie_theatre/Movies/antman2.jpg" class="rounded-5" alt="antman2" width="170"
+          height="230px" id="box-shadow">
+      </div>
+      <div class="col-md-1">
+        <img src="./assets/image/movie_theatre/Movies/avengers.jpg" class="rounded-5" alt="avengers" width="170"
+          height="230px" id="box-shadow">
+      </div>
+      <div class="col-md-1">
+        <img src="./assets/image/movie_theatre/Movies/avengers2.jpg" class="rounded-5" alt="avengers2" width="170"
+          height="230px" id="box-shadow">
+      </div>
+    </div>
+    </div>
+    </div>
+  </section>
+
+
+  <section>
+    <div>
+      <p class="h2 bold uppercase"> VOD</p>
+      <div class="d-flex justify-content-center">
+        <div class="col-md-1">
+          <img src="./assets/image/movie_theatre/Movies/antman.jpg" class="rounded-5" alt="antman" width="170"
+            height="230" id="box-shadow">
+        </div>
+        <div class="col-md-1">
+          <img src="./assets/image/movie_theatre/Movies/antman2.jpg" class="rounded-5" alt="antman2" width="170"
+            height="230px" id="box-shadow">
+        </div>
+        <div class="col-md-1">
+          <img src="./assets/image/movie_theatre/Movies/avengers.jpg" class="rounded-5" alt="avengers" width="170"
+            height="230px" id="box-shadow">
+        </div>
+        <div class="col-md-1">
+          <img src="./assets/image/movie_theatre/Movies/avengers2.jpg" class="rounded-5" alt="avengers2" width="170"
+            height="230px" id="box-shadow">
+        </div>
+        <div class="col-md-1">
+          <img src="./assets/image/movie_theatre/Movies/antman.jpg" class="rounded-5" alt="antman" width="170"
+            height="230" id="box-shadow">
+        </div>
+        <div class="col-md-1">
+          <img src="./assets/image/movie_theatre/Movies/antman2.jpg" class="rounded-5" alt="antman2" width="170"
+            height="230px" id="box-shadow">
+        </div>
+        <div class="col-md-1">
+          <img src="./assets/image/movie_theatre/Movies/avengers.jpg" class="rounded-5" alt="avengers" width="170"
+            height="230px" id="box-shadow">
+        </div>
+        <div class="col-md-1">
+          <img src="./assets/image/movie_theatre/Movies/avengers2.jpg" class="rounded-5" alt="avengers2" width="170"
+            height="230px" id="box-shadow">
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
+
+
+  <section>
+    <div>
+      <p class="h2 bold uppercase"> STREAMING</p>
+      <div class="d-flex justify-content-between">
+        <div class="col-md-1">
+          <img src="./assets/image/movie_theatre/Movies/antman.jpg" class="rounded-5" alt="antman" width="170"
+            height="230" id="box-shadow">
+        </div>
+        <div class="col-md-1">
+          <img src="./assets/image/movie_theatre/Movies/antman2.jpg" class="rounded-5" alt="antman2" width="170"
+            height="230px" id="box-shadow">
+        </div>
+        <div class="col-md-1">
+          <img src="./assets/image/movie_theatre/Movies/avengers.jpg" class="rounded-5" alt="avengers" width="170"
+            height="230px" id="box-shadow">
+        </div>
+        <div class="col-md-1">
+          <img src="./assets/image/movie_theatre/Movies/avengers2.jpg" class="rounded-5" alt="avengers2" width="170"
+            height="230px" id="box-shadow">
+        </div>
+        <div class="col-md-1">
+          <img src="./assets/image/movie_theatre/Movies/antman.jpg" class="rounded-5" alt="antman" width="170"
+            height="230" id="box-shadow">
+        </div>
+        <div class="col-md-1">
+          <img src="./assets/image/movie_theatre/Movies/antman2.jpg" class="rounded-5" alt="antman2" width="170"
+            height="230px" id="box-shadow">
+        </div>
+        <div class="col-md-1">
+          <img src="./assets/image/movie_theatre/Movies/avengers.jpg" class="rounded-5" alt="avengers" width="170"
+            height="230px" id="box-shadow">
+        </div>
+        <div class="col-md-1">
+          <img src="./assets/image/movie_theatre/Movies/avengers2.jpg" class="rounded-5" alt="avengers2" width="170"
+            height="230px" id="box-shadow">
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
+  </main> -->
+
+
+<?php include '_partials/footer.php'; ?>
+
+
+</body>
+</html>
